742ec42686e75139baa9837bc4456b636517ef13
Santosh344 <khatrisantos3@gmail.com>
2019-04-14 21:26:01 +0545

    Android task 4

---
+++ Task4/app/src/main/java/com/example/task4/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.task4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etCarName,etYear,etColor,etPurchasePrice,etEngineSize;
+    private Button btnCreateCar,btnCreateDiesel;
+    private TextView tvOutput;
+    private  int countvehicle = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etCarName = (EditText) findViewById(R.id.etCarName);
+        etYear = (EditText) findViewById(R.id.etYear);
+        etColor = (EditText) findViewById(R.id.etColor);
+        etPurchasePrice = (EditText) findViewById(R.id.etPurchasePrice);
+        etEngineSize = (EditText) findViewById(R.id.etEngineSize);
+        tvOutput = (TextView) findViewById(R.id.tvOutput);
+        btnCreateCar = (Button) findViewById(R.id.btnCreateCar);
+
+        btnCreateCar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!validate()) {
+                    return;
+                }
+
+                String name = etCarName.getText().toString();
+                int year = Integer.parseInt(etYear.getText().toString());
+                String color = etColor.getText().toString();
+                float engine = Float.parseFloat(etEngineSize.getText().toString());
+                float price = Float.parseFloat(etPurchasePrice.getText().toString());
+                countvehicle++;
+                String heading = "Vehicle counted: " + countvehicle + "\n";
+                String content = "Manufacture : " + name + " | Year : " + year + " | Color :" + color +
+                        " | Engine :" + engine + " | Price :" + price + "\n\n";
+                tvOutput.append(heading + content);
+            }
+
+            private boolean validate() {
+
+                if (TextUtils.isEmpty(etCarName.getText().toString())) {
+                    etCarName.setError("Enter Car Name");
+                    etCarName.requestFocus();
+                    return false;
+                } else if (TextUtils.isEmpty(etColor.getText().toString())) {
+                    etColor.setError("enter car name");
+                    etColor.requestFocus();
+                    return false;
+                } else if (TextUtils.isEmpty(etYear.getText().toString())) {
+                    etYear.setError("Enter car Name");
+                    etYear.requestFocus();
+                    return false;
+                } else if (TextUtils.isEmpty(etPurchasePrice.getText().toString())) {
+                    etPurchasePrice.setError("Enter car Name");
+                    etPurchasePrice.requestFocus();
+                    return false;
+                } else if (TextUtils.isEmpty(etEngineSize.getText().toString())) {
+                    etEngineSize.setError("Enter car Name");
+                    etEngineSize.requestFocus();
+                    return false;
+                }
+                return true;
+            }
+        });
+    }
+}
